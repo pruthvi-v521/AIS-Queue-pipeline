9ab366c007beff7a00265661766830e82ab6f70c
Sarvesh Dhumal <72063635+Sarveshdhumal@users.noreply.github.com>
2026-03-27 19:13:47 +0100

    Update database URL for PostgreSQL connection

--- api/app/database.py
+++ api/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://appuser:apppassword@localhost:5432/ais"
+SQLALCHEMY_DATABASE_URL = "postgresql://appuser:password@host.docker.internal:5432/ais"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
